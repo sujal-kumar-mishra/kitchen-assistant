bf157326a5915e850d2a78a50f09dff5ad9971c4
sujal <sujalkumar123479@gmail.com>
2025-09-06 15:31:03 +0530

    backend_url_upated

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
 // change this if your backend is at a different url (e.g., deployed)
-export const FRONTEND_BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
+export const FRONTEND_BACKEND_URL = "https://kitchen-assistant-s12i.onrender.com";
